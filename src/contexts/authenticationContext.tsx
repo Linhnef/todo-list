4f4f797648c63b5d9e3e0e7d093f3423507ee946
Linhnef <begin270519@gmail.com>
2021-07-21 10:39:57 +0700

    addTask feature UI- api- context

--- src/contexts/authenticationContext.tsx
+++ src/contexts/authenticationContext.tsx
@@ -1,4 +1,5 @@
 import { useState, createContext, ReactNode } from "react"
+import { Task } from "../services/api/types/Task"
 import { User } from "../services/api/types/User"
 
 interface AuthenticationContextProps {
@@ -6,6 +7,8 @@ interface AuthenticationContextProps {
   setUser: (data: User) => void
   token: string | undefined | null
   setToken: (data: string | null) => void
+  tasks: Task[] | null | undefined
+  setTasks: (tasks: Task[]) => void
 }
 
 export const AuthenticationContext = createContext<AuthenticationContextProps>({
@@ -13,6 +16,8 @@ export const AuthenticationContext = createContext<AuthenticationContextProps>({
   setUser: (data: User | null) => {},
   token: null,
   setToken: (data: string | null) => {},
+  tasks: null,
+  setTasks: (data: Task[]) => {},
 })
 interface AuthenticationContextProviderProps {
   children: ReactNode
@@ -21,13 +26,16 @@ interface AuthenticationContextProviderProps {
 export const AuthenticationContextProvider = (props: AuthenticationContextProviderProps) => {
   const [user, setUser] = useState<User | null>(null)
   const [token, setToken] = useState<string | null>(null)
+  const [tasks, setTasks] = useState<Task[] | null>(null)
   token ? localStorage.setItem("token", token) : localStorage.clear()
 
   const contextValue: AuthenticationContextProps = {
     user: user,
     token: token,
+    tasks: tasks,
     setUser,
     setToken,
+    setTasks,
   }
   return <AuthenticationContext.Provider value={contextValue}>{props.children}</AuthenticationContext.Provider>
 }
---
+++ src/pages/Task.tsx
@@ -0,0 +1,106 @@
+import { AppBar, Grid, Toolbar, Badge, Typography, IconButton, Dialog, Paper } from "@material-ui/core"
+import styled from "styled-components"
+import NoteAddIcon from "@material-ui/icons/NoteAdd"
+import { InputOutlined } from "../components/inputs/InputOutlined"
+import { ButtonOutlined } from "../components/buttons/ButtonOutlined"
+import { useState } from "react"
+import { useAppApiClient } from "../hooks/useAppApiClient"
+import useAsync from "../hooks/useAsync"
+import { AddTaskRequest } from "../services/api/createAppApiClient"
+
+const TaskTodo = () => {
+  const api = useAppApiClient()
+  const [isAddOpen, setIsAddOpen] = useState(false)
+  const [addTaskDescription, setAddTaskDescription] = useState<string>("")
+  const addTask = useAsync<boolean | undefined | null, AddTaskRequest>(api.addTask)
+  const addTaskHandle = async () => {
+    const result = await addTask.run({ description: addTaskDescription })
+    if (!result) return
+    setAddTaskDescription("")
+    setIsAddOpen(false)
+  }
+  return (
+    <>
+      <TaskTodoHeader color="default" position="static">
+        <Toolbar>
+          <Grid container>
+            <TaskTodoHeaderGrid item sm={12}>
+              <TaskTodoHeaderBackground item>
+                <Typography variant="h3">TODO LIST</Typography>
+              </TaskTodoHeaderBackground>
+              <TaskTodoListIcon
+                onClick={() => {
+                  setIsAddOpen(true)
+                }}
+              >
+                <Badge color="secondary">
+                  <NoteAddIcon fontSize="large" />
+                </Badge>
+              </TaskTodoListIcon>
+            </TaskTodoHeaderGrid>
+          </Grid>
+        </Toolbar>
+      </TaskTodoHeader>
+      <Dialog open={isAddOpen}>
+        <AddPaper>
+          <AddInput
+            value={addTaskDescription}
+            onChange={(event: any) => {
+              setAddTaskDescription(event.target.value)
+            }}
+            label="Description"
+          />
+          <ButtonOutlined onClick={addTaskHandle}>Submit</ButtonOutlined>
+          <ButtonOutlined
+            onClick={() => {
+              setIsAddOpen(false)
+            }}
+          >
+            Cancle
+          </ButtonOutlined>
+        </AddPaper>
+      </Dialog>
+    </>
+  )
+}
+
+const TaskTodoHeader = styled(AppBar)`
+  width: 100%;
+  background-color: white;
+  margin-bottom: 5%;
+`
+
+const TaskTodoHeaderGrid = styled(Grid)`
+  border: 1px solid;
+`
+
+const TaskTodoListIcon = styled(IconButton)`
+  float: right;
+  display: block;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const TaskTodoHeaderBackground = styled(Grid)`
+  display: block;
+  padding-top: 0.5%;
+  padding-left: 1%;
+  float: left;
+`
+const IcoinButtonTable = styled(IconButton)`
+  float: right;
+  &:hover {
+    cursor: pointer;
+  }
+`
+
+const AddPaper = styled(Paper)`
+  padding: 100px;
+`
+
+const AddInput = styled(InputOutlined)`
+  width: 90%;
+`
+
+export default TaskTodo
