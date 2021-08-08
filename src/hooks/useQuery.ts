eb0d7b69cd37aa7f419d4d7861be5c0f68ca1f58
Nguyen Bao Linh <63001824+Linhnef@users.noreply.github.com>
2021-08-08 21:55:13 +0700

    const { parse, stringify } = queryStringLib  ---> import { parse, stringify } from "query-string"

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,11 +1,10 @@
-import * as queryStringLib from "query-string"
-import { useCallback, useEffect, useState } from "react"
+import { parse, stringify } from "query-string"
+import { useEffect, useState } from "react"
 import { useHistory, useLocation } from "react-router"
 
 const useQuery = <TQuery>(initialQuery: TQuery) => {
   const history = useHistory()
   const queryString = useLocation().search
-  const { parse, stringify } = queryStringLib
   const [query, setQuery] = useState<TQuery | any>(initialQuery)
   const patchQuery = (obj: TQuery) => {
     history.push({ search: stringify(obj) })
