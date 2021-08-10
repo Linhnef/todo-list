fdeb0af099334c374a7001b594ed66a69566157b
Nguyen Bao Linh <63001824+Linhnef@users.noreply.github.com>
2021-08-10 13:20:23 +0700

    parse(queryString) as unknown as TQuery -->  parse(queryString) as unknown as TQuery ? parse(queryString) as unknown as TQuery  : initialQuery

--- src/hooks/useQuery.ts
+++ src/hooks/useQuery.ts
@@ -1,17 +1,12 @@
 import { parse, stringify } from "query-string"
-import { useEffect, useState } from "react"
 import { useHistory, useLocation } from "react-router"
 
 const useQuery = <TQuery>(initialQuery: TQuery) => {
   const history = useHistory()
   const queryString = useLocation().search
-  const queryParams = parse(queryString) as unknown as TQuery
-  const [query, setQuery] = useState<TQuery>(initialQuery)
+  const query = parse(queryString) as unknown as TQuery ? parse(queryString) as unknown as TQuery  : initialQuery 
   const patchQuery = (updatedQuery: TQuery) => {
     history.push({ search: stringify(updatedQuery) })
-  }
-  if(query !== queryParams){
-    setQuery(queryParams)
   }
   return { query, patchQuery }
 }
