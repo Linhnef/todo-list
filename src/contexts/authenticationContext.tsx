8b8c58e1ca2de5ca1e078dac41215db8d303ceb9
Linhnef <begin270519@gmail.com>
2021-08-18 16:43:51 +0700

    upload Image

--- src/contexts/authenticationContext.tsx
+++ src/contexts/authenticationContext.tsx
@@ -2,17 +2,21 @@ import { useState, createContext, ReactNode } from "react"
 import { User } from "../services/api/types/User"
 
 interface AuthenticationContextProps {
+  userAvatar: string | null | undefined
   user: User | null | undefined
   setUser: (data: User) => void
   token: string | undefined | null
   setToken: (data: string | null) => void
+  setUserAvatar: (data: string) => void
 }
 
 export const AuthenticationContext = createContext<AuthenticationContextProps>({
+  userAvatar: null,
   user: null,
   setUser: (data: User | null) => {},
   token: null,
   setToken: (data: string | null) => {},
+  setUserAvatar: (data: string) => {},
 })
 interface AuthenticationContextProviderProps {
   children: ReactNode
@@ -21,6 +25,7 @@ interface AuthenticationContextProviderProps {
 export const AuthenticationContextProvider = (props: AuthenticationContextProviderProps) => {
   const [user, setUser] = useState<User | null>(null)
   const [token, setToken] = useState<string | null>(null)
+  const [userAvatar, setUserAvatar] = useState<string | null>(null)
   token ? localStorage.setItem("token", token) : localStorage.clear()
 
   const contextValue: AuthenticationContextProps = {
@@ -28,6 +33,8 @@ export const AuthenticationContextProvider = (props: AuthenticationContextProvid
     token: token,
     setUser,
     setToken,
+    userAvatar,
+    setUserAvatar,
   }
   return <AuthenticationContext.Provider value={contextValue}>{props.children}</AuthenticationContext.Provider>
 }
--- src/pages/UpdateUser.tsx
+++ src/pages/UpdateUser.tsx
@@ -1,23 +1,25 @@
-import { Grid, Typography } from "@material-ui/core"
+import { Avatar, FormGroup, Badge, IconButton, Typography } from "@material-ui/core"
 import * as React from "react"
 import { ButtonOutlined } from "../components/buttons/ButtonOutlined"
 import { useContext, useState, ChangeEvent } from "react"
 import { AuthenticationContext } from "../contexts/authenticationContext"
 import { useHistory } from "react-router"
 import styled from "styled-components"
-import { UpdateCurrentUserRequest, UpdateCurrentUserResponse } from "../services/api/createAppApiClient"
+import { UpdateCurrentUserRequest } from "../services/api/createAppApiClient"
 import { useAppApiClient } from "../hooks/useAppApiClient"
 import useAsync from "../hooks/useAsync"
 import { InputOutlined } from "../components/inputs/InputOutlined"
+import AddAPhotoIcon from "@material-ui/icons/AddAPhoto"
 
 export const UpdateUser = () => {
   const history = useHistory()
   const api = useAppApiClient()
-  const { user, setUser } = useContext(AuthenticationContext)
+  const { user, setUser, setUserAvatar, userAvatar } = useContext(AuthenticationContext)
   const [name, setName] = useState<string>()
   const [age, setAge] = useState<number>()
   const [email, setEmail] = useState<string>()
-
+  const [preview, setPreview] = useState<string | null>(userAvatar ? userAvatar : null)
+  const [img, setImg] = useState<File>()
 
   const updateUser = useAsync(async (updateUserRequest: UpdateCurrentUserRequest) => {
     const response = await api.updateCurrentUser(updateUserRequest)
@@ -26,42 +28,79 @@ export const UpdateUser = () => {
     history.replace("/")
   })
 
+  const updateImage = useAsync(async (data: FormData) => {
+    const response = await api.uploadImage(data)
+    if (!response) return
+  })
+
+  const handleUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+    const file = event.currentTarget.files as unknown as File[]
+    if (!file) return
+    setImg(file[0])
+    setPreview(URL.createObjectURL(file[0]))
+  }
+
+  const handleUpdate = () => {
+    if (img && preview !== null) {
+      const formData = new FormData()
+      formData.append("avatar", img)
+      updateImage.run(formData)
+      setUserAvatar(preview)
+    }
+
+    updateUser.run({ age, name, email })
+  }
+
   return (
     <React.Fragment>
       {updateUser.error === null && user ? (
-        <FormControl container>
-          <ProfileGrid alignItems="center" container direction="column">
-            <h2>Profile</h2>
-          </ProfileGrid>
+        <>
+          <Container>
+            <InformationContainer>
+              <Badge
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "right",
+                }}
+                badgeContent={
+                  <label htmlFor="btn-upload">
+                    <ChooseFile id="btn-upload" onChange={handleUpload} accept="image/*" type="file" />
+                    <IconButton component="span">
+                      <AddAPhotoIcon />
+                    </IconButton>
+                  </label>
+                }
+              >
+                <Avatar src={preview ? preview : ""} />
+              </Badge>
 
-          <FormGrid item xs={2}>
-            <InputOutlined
-              label="Name"
-              onChange={(event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)}
-              defaultValue={user.name}
-              value={name}
-            />
+              <InputOutlined
+                label="Name"
+                onChange={(event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)}
+                defaultValue={user.name}
+                value={name}
+              />
 
-            <InputOutlined
-              label="Age"
-              onChange={(event: ChangeEvent<HTMLInputElement>) => setAge(parseInt(event.target.value))}
-              defaultValue={user.age}
-              value={age}
-              type="number"
-            />
+              <InputOutlined
+                label="Age"
+                onChange={(event: ChangeEvent<HTMLInputElement>) => setAge(parseInt(event.target.value))}
+                defaultValue={user.age}
+                value={age}
+                type="number"
+              />
 
-            <InputOutlined
-              label="label"
-              onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
-              defaultValue={user.email}
-              value={email}
-            />
-            <ButtonOutlined color="primary" onClick={() => updateUser.run({ age, name, email })}>
-
-              Update
-            </ButtonOutlined>
-          </FormGrid>
-        </FormControl>
+              <InputOutlined
+                label="label"
+                onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+                defaultValue={user.email}
+                value={email}
+              />
+              <ButtonOutlined color="primary" onClick={() => handleUpdate()}>
+                Update
+              </ButtonOutlined>
+            </InformationContainer>
+          </Container>
+        </>
       ) : updateUser.loading ? (
         <Typography variant="h2">Loading</Typography>
       ) : (
@@ -71,14 +110,26 @@ export const UpdateUser = () => {
   )
 }
 
-const ProfileGrid = styled(Grid)`
-  text-align: center;
-  width: 100%;
+const ChooseFile = styled.input`
+  display: none;
 `
 
-const FormGrid = styled(Grid)`
-  margin-left: 45%;
-  width: 80%;
+const InformationContainer = styled(FormGroup)`
+  justify-content: center;
+  align-items: center;
+  & div {
+    margin: 0.2em 0;
+  }
+  & .MuiAvatar-root {
+    height: 5em;
+    width: 5em;
+    border: 1px solid black;
+  }
 `
 
-const FormControl = styled(Grid)``
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`
