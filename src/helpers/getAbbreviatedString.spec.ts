1c74545f7de6335e01f1bd3fcd45c11bc4934de0
Linhnef <begin270519@gmail.com>
2021-09-07 10:46:31 +0700

    add test getAbbreviatedString

--- src/helpers/getAbbreviatedString.spec.ts
+++ src/helpers/getAbbreviatedString.spec.ts
@@ -1,12 +1,12 @@
 import { getAbbreviatedString } from "./getAbbreviatedString"
 
-describe("Get Abbreviated String", () => {
-  it("it should be return first and last word of string", () => {
+describe("Get abbreviated string", () => {
+  it("it should be return first character of the first and last words of the string", () => {
     expect(getAbbreviatedString("Trần Dần")).toEqual("TD")
     expect(getAbbreviatedString("Nguyễn Bảo Linh")).toEqual("LB")
     expect(getAbbreviatedString("Phạm Thị Mỹ Nhân")).toEqual("PN")
   })
-  it("it should be return the firt character  of word", () => {
+  it("it should be return the first character of word", () => {
     expect(getAbbreviatedString("Linh")).toEqual("L")
   })
 })
