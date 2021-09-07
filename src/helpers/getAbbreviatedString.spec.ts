38c1e174334e669456490289ed915386832582f7
Linhnef <begin270519@gmail.com>
2021-09-07 10:12:44 +0700

    update test getAbbreviatedString funtion

--- src/helpers/getAbbreviatedString.spec.ts
+++ src/helpers/getAbbreviatedString.spec.ts
@@ -2,9 +2,9 @@ import { getAbbreviatedString } from "./getAbbreviatedString"
 
 describe("Get Abbreviated String", () => {
   it("it should be return first and last word of string", () => {
-    expect(getAbbreviatedString("Phạm C")).toEqual("PC")
-    expect(getAbbreviatedString("Lê Văn B")).toEqual("LB")
-    expect(getAbbreviatedString("Nguyễn Văn Thanh A")).toEqual("NA")
+    expect(getAbbreviatedString("Trần Dần")).toEqual("TD")
+    expect(getAbbreviatedString("Nguyễn Bảo Linh")).toEqual("LB")
+    expect(getAbbreviatedString("Phạm Thị Mỹ Nhân")).toEqual("PN")
   })
   it("it should be return the firt character  of word", () => {
     expect(getAbbreviatedString("Linh")).toEqual("L")
