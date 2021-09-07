52fb3d7a2e90273ed8c7864b5676792b15cc5987
Linhnef <begin270519@gmail.com>
2021-09-07 09:58:42 +0700

    update test getAbbreviatedString funtion

--- src/helpers/getAbbreviatedString.spec.ts
+++ src/helpers/getAbbreviatedString.spec.ts
@@ -2,9 +2,9 @@ import { getAbbreviatedString } from "./getAbbreviatedString"
 
 describe("Get Abbreviated String", () => {
   it("it should be return first and last word of string", () => {
-    expect(getAbbreviatedString("Nguyễn Văn Thanh A")).toEqual("NA")
-    expect(getAbbreviatedString("Lê Văn B")).toEqual("LB")
     expect(getAbbreviatedString("Phạm C")).toEqual("PC")
+    expect(getAbbreviatedString("Lê Văn B")).toEqual("LB")
+    expect(getAbbreviatedString("Nguyễn Văn Thanh A")).toEqual("NA")
   })
   it("it should be return the firt character  of word", () => {
     expect(getAbbreviatedString("Linh")).toEqual("L")
